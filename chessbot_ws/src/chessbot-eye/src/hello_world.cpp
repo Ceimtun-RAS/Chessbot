86c44590d508070cde271b378e2cc07e6d677565
Andrés Morales <amoralesma@unal.edu.co>
2023-05-07 12:18:57 -0500

    [Distance]

--- chessbot_ws/src/chessbot-eye/src/hello_world.cpp
+++ chessbot_ws/src/chessbot-eye/src/hello_world.cpp
@@ -4,12 +4,12 @@
 #include <librealsense2/rs.hpp> // Include Intel RealSense Cross Platform API
 
 #include "ros/ros.h"
-#include "sensor_mgsg/PointCloud2.h"
+#include "sensor_msgs/PointCloud2.h"
 
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "sensor")
-
+    ros::init(argc, argv, "sensor");
+    ros::NodeHandle nh; 
     ROS_INFO("comenzando nodo!"); 
     // Create a Pipeline - this serves as a top-level API for streaming and processing frames
     rs2::pipeline p;
@@ -18,19 +18,26 @@ int main(int argc, char** argv){
     // Configure and start the pipeline
     p.start();
     // Block program until frames arrive
-    rs2::frameset frames = p.wait_for_frames();
-    // Try to get a frame of a depth image
-    rs2::depth_frame depth = frames.get_depth_frame();
+    ROS_INFO("wainting for stream!"); 
+    
+    ros::Rate r = ros::Rate(100);
+
+    while(ros::ok()){
+    	rs2::frameset frames = p.wait_for_frames();
+    	ROS_INFO("stream received..."); 
+    	// Try to get a frame of a depth image
+    	rs2::depth_frame depth = frames.get_depth_frame();
 
         // Get the depth frame's dimensions
-    float width = depth.get_width();
-    float height = depth.get_height();
-    // Query the distance from the camera to the object in the center of the image
-    float dist_to_center = depth.get_distance(width / 2, height / 2);
-
-    // Print the distance
-    std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
-    ros::spin(); 
+    	float width = depth.get_width();
+    	float height = depth.get_height();
+    	// Query the distance from the camera to the object in the center of the image
+    	float dist_to_center = depth.get_distance(width / 2, height / 2);
+	
+	std::cout << "Los datos de la realsense son: " << dist_to_center << std::endl;  
+	
+	r.sleep(); 
+    }
     
     return 1; 
 }
