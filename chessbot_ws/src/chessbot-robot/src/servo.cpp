392dacccb61c59107e0aac4c1f59b30666f17f3b
Andrés Morales <amoralesma@unal.edu.co>
2023-05-06 22:12:59 -0500

    [Updarte]

--- chessbot_ws/src/chessbot-robot/src/servo.cpp
+++ chessbot_ws/src/chessbot-robot/src/servo.cpp
@@ -12,12 +12,14 @@ int main(int argc, char** argv){
     int range = 100; // Establece el rango de valores PWM
     int divisor = 500; // Establece el divisor de reloj para la señal PWM
 
+    ROS_INFO("Antes del pinMode"); 
     pinMode(pin, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
     pwmSetRange(range);
     pwmSetClock(divisor);
     
     int dutyCycle = 7; // Reemplaza con el ciclo de trabajo PWM adecuado para mover el servo a la posición deseada
+    ROS_INFO("Antes del write"); 
     pwmWrite(pin, dutyCycle);
     delay(1000); // Espera un segundo antes de enviar la siguiente señal
 
