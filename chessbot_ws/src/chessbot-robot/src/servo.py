fa814e6c4fe896ebc0ace7545bcf84d063f22e2a
Andrés Morales <amoralesma@unal.edu.co>
2023-05-06 22:52:31 -0500

    [send]

---
+++ chessbot_ws/src/chessbot-robot/src/servo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import rospy
+import RPi.GPIO as GPIO
+import time 
+
+PIN_MOT = 11
+
+def main():
+    rospy.init_node('prueba')
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN_MOT, GPIO.OUT)
+
+    servo = GPIO.PWM(PIN_MOT, 50) 
+
+    servo.start(0)
+    print("Waiting for 1 second") 
+    time.sleep(1)
+
+    print("Rotating at intervals of 12 deg")
+    duty = 2
+    while duty <= 17:
+        servo.ChangeDutyCycle(duty)
+        time.sleep(1)
+        duty = duty + 1
+
+    servo.stop()
+    GPIO.cleanup()
+    print("Done")
+
+
+if __name__ == '__main__':
+    main()
+
