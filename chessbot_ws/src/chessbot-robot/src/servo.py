05b4e7425672af2d17aa3884a5190de9835ed55f
Andrés Morales <amoralesma@unal.edu.co>
2023-05-06 23:30:51 -0500

    []

--- chessbot_ws/src/chessbot-robot/src/servo.py
+++ chessbot_ws/src/chessbot-robot/src/servo.py
@@ -5,11 +5,16 @@ import RPi.GPIO as GPIO
 import time 
 
 PIN_MOT = 11
-
+active_servo = True
 
 def change_velocity(data, servo):
+
     rospy.loginfo("Moviendo servo a {data.data}")
-    servo.ChangeDutyCycle(data.data)
+    if active_servo:
+        servo.ChangeDutyCycle(data.data)
+    else:
+        rospy.loginfo("servo inactivo. Activalo para moverlo.")
+
 
 def close(servo):
     servo.stop()
@@ -19,6 +24,8 @@ def main():
     rospy.init_node('myServo')
     rospy.loginfo("Configurando servo...")
     rospy.loginfo("comenzando"); 
+    r = rospy.Rate(10)
+    
     # main config servo 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PIN_MOT, GPIO.OUT)
@@ -26,10 +33,26 @@ def main():
     servo = GPIO.PWM(PIN_MOT, 50) 
 
     servo.start(0)
+    
 
     rospy.Subscriber("cmd_servo", Int16, change_velocity, servo)
     
-    rospy.spin()
+    while not rospy.is_shutdown():
+        if rospy.has_param("/is_active_servo"):
+            active_servo = rospy.get_param("/is_active_servo")
+            
+            if active_servo == False:
+                servo.ChangeDutyCycle(0)
+        else: 
+            rospy.loginfo("activando servo...")
+            rospy.set_param('/is_active_servo', True)
+
+        r.sleep()
+        
+
+
+
+    
 
 
 if __name__ == '__main__':
