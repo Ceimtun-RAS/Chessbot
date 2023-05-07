f30671d4b3be362c24da7dd736fcf39398cb628f
Andrés Morales <amoralesma@unal.edu.co>
2023-05-06 23:17:15 -0500

    [u`date]

--- chessbot_ws/src/chessbot-robot/src/servo.py
+++ chessbot_ws/src/chessbot-robot/src/servo.py
@@ -7,7 +7,7 @@ import time
 PIN_MOT = 11
 
 
-def change_velocity(data):
+def change_velocity(data, servo):
     rospy.loginfo("Moviendo servo a {data.data}")
     servo.ChangeDutyCycle(data.data)
 
@@ -18,13 +18,6 @@ def close(servo):
 def main():
     rospy.init_node('myServo')
     rospy.loginfo("Configurando servo...")
-
-    rospy.Subscriber("cmd_servo", Int16, change_velocity)
-    
-    rospy.spin()
-
-
-if __name__ == '__main__':
     rospy.loginfo("comenzando"); 
     # main config servo 
     GPIO.setmode(GPIO.BCM)
@@ -33,5 +26,13 @@ if __name__ == '__main__':
     servo = GPIO.PWM(PIN_MOT, 50) 
 
     servo.start(0)
+
+    rospy.Subscriber("cmd_servo", Int16, change_velocity, servo)
+    
+    rospy.spin()
+
+
+if __name__ == '__main__':
+  
     main()
 
